74f0afa304ab1ccbc3d4c2fbd9a17c7a7f42fb1d
ĐαɾƙSσυʅ ‘ ∂¡૯ <78919907+Darksoul8@users.noreply.github.com>
2021-03-16 13:58:12 -0300

    Update update.sh

--- core/update.sh
+++ core/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf *.py
+rm -rf core
 git clone https://github.com/Darksoul8/HunterSearch
+git clone https://github.com/Darksoul8/HunterSearch/core
 mv ./HunterSearch/* ./
 rm -rf HunterSearch 
 chmod +x *
